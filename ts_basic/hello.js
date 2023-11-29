01920bdee8f88bc0bd0e761a3b2509981404a478
220106 <220106rq@yse-c.net>
2023-11-29 11:37:41 +0900

    main

---
+++ ts_basic/hello.js
@@ -0,0 +1,16 @@
+var message = "Hello!!!!";
+console.log(message);
+//Type Inference
+var id = 1;
+var email = "test@test.com";
+var isActive = true;
+var none;
+//Type Definition
+// var id:number = 1;
+// var email:string = "test@test.com";
+// var isActive:boolean = true;
+// var none : any;
+console.log(id, typeof id);
+console.log(email, typeof email);
+console.log(isActive, typeof isActive);
+console.log(none, typeof none);
---
+++ ts_basic/hello.ts
@@ -0,0 +1,20 @@
+var message: string = "Hello!!!!";
+console.log(message);
+
+//Type Inference
+var id = 1;
+var email = "test@test.com";
+var isActive = true;
+var none;
+
+//Type Definition
+// var id:number = 1;
+// var email:string = "test@test.com";
+// var isActive:boolean = true;
+// var none : any;
+
+console.log(id, typeof id)
+console.log(email, typeof email)
+console.log(isActive, typeof isActive)
+console.log(none, typeof none)
+
