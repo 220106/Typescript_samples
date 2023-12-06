38ccda3eb0f303a32ba922017164453be079b3de
220106 <220106rq@yse-c.net>
2023-12-06 11:10:45 +0900

    update

--- ts_basic/items.ts
+++ ts_basic/items.ts
@@ -1,5 +1,5 @@
 // 配列
-var itemNames = ["コーヒー", "紅茶", "ほうじ茶"];
+var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
 console.log(itemNames);
 
 //オブジェクト配列
@@ -8,34 +8,45 @@ var item1 = {
     name: "コーヒー",
     price: 350,
 }
-
 var item2 = {
     id: 2,
     name: "紅茶",
     price: 400,
 }
-
 var item3 = {
     id: 3,
     name: "ほうじ茶",
     price: 300,
 }
 
-//var items:object[] = [item1, item2, item3]
-
-var items = [item1, item2, item3]
+// var items:object[] = [item1, item2, item3];
+var items = [item1, item2, item3];
 console.log(items);
 
 var borderPrice = 350;
 var filterItems = items.filter((item) => item.price <= borderPrice);
-console.log(filterItems)
+console.log(filterItems);
 
 //function
-function calculateTotalPrice(price: number, amount: number): number {
-    var totarPrice = price * amount;
-    return totarPrice
+// function calculateTotalPrice(price:number, amount:number): number {
+//    var totalPrice = price * amount; 
+//    return totalPrice;
+// }
+//anonymus function
+// const calculateTotalPrice = function(price:number, amount:number): number {
+//    var totalPrice = price * amount; 
+//    return totalPrice;
+// }
+//arrow function
+const calculateTotalPrice = (price:number, amount:number): number => {
+   var totalPrice = price * amount; 
+   return totalPrice;
+}
+
+function findItem(id:number) {
+    return items.find((item) => item.id == id);
 }
 
 var amount = 5;
-var totalPrice = calculateTotalPrice(item1.price, amount)
-console.log(totalPrice)
+var totalPrice = calculateTotalPrice(item1.price, amount);
+console.log(totalPrice);
